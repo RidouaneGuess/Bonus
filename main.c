5a4f47745e00d332550edba34fba95ee4789a08e
Ridouane <ridouaneaboubacarvisio8092@gmail.com>
2022-11-12 23:48:10 +0100

    docs:correction de la ligne8

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
         char *nom;
         nom=(char*)malloc(sizeof(char));
         printf("\nEntrez votre nom:");
-        scanf(nom);
+        scanf("%s",nom);
         printf("Votre nom est:%s",nom);
         return 0;
 }
