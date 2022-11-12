4bf64b129ea4c12a62deb146db2bc1f3470de0d1
Ridouane <ridouaneaboubacarvisio8092@gmail.com>
2022-11-12 23:15:19 +0100

    feat:Ridouane

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include stdlib.h
+int main()
+{
+        char nom;
+        nom=(char*)malloc(sizeof(char));
+        printf("\nEntrez votre nom:");
+        scanf(nom);
+        printf("Votre nom est:%s",nom);
+        return 0;
+}
